115cf9e7a5816cae13af7382aad736e8a8d0db9c
Nemanja Ilic <nemanja.ilic@outlook.com>
2017-03-30 19:24:01 +0200

    Percentage calculation done

--- RenderedPizza/ViewModels/MainWindowViewModel.cs
+++ RenderedPizza/ViewModels/MainWindowViewModel.cs
@@ -24,6 +24,48 @@ namespace RenderedPizza.ViewModels
 		private List<PizzaModel> pizzasWithMeatAndOlives;
 		private List<PizzaModel> pizzasWithMozzarelaAndMushrooms;
 
+		private double pizzasWithMeatPercentage;
+		public double PizzasWithMeatPercentage
+		{
+			get { return pizzasWithMeatPercentage; }
+			set
+			{
+				pizzasWithMeatPercentage = value;
+			}
+		}
+		private double pizzasWithMultipleCheasePercentage;
+		public double PizzasWithMultipleCheasePercentage
+		{
+			get { return pizzasWithMultipleCheasePercentage; }
+			set
+			{
+				pizzasWithMultipleCheasePercentage = value;
+			}
+		}
+		private double pizzasWithMeatAndOlivesPercentage;
+		public double PizzasWithMeatAndOlivesPercentage
+		{
+			get { return pizzasWithMeatAndOlivesPercentage; }
+			set
+			{
+				pizzasWithMeatAndOlivesPercentage = value;
+			}
+		}
+		private double pizzasWithMozzarelaAndMushroomsPercentage;
+		public double PizzasWithMozzarelaAndMushroomsPercentage
+		{
+			get { return pizzasWithMozzarelaAndMushroomsPercentage; }
+			set
+			{
+				pizzasWithMozzarelaAndMushroomsPercentage = value;
+			}
+		}
+
+		private PizzaModel cheapestPizzaWithMeat;
+		private PizzaModel cheapestPizzaWithMultipleChease;
+		private PizzaModel cheapestPizzaWithMeatAndOlives;
+		private PizzaModel cheapestPizzaWithMozzarelaAndMushrooms;
+
 		private List<PizzaModel> pizzas;
 		public List<PizzaModel> Pizzas
 		{
@@ -49,6 +91,22 @@ namespace RenderedPizza.ViewModels
 			pizzasWithMultipleChease = PizzasRepacker.getPizzasWithMultipleChease(pizzas);
 			pizzasWithMeatAndOlives = PizzasRepacker.getPizzasWithMeatAndOlives(pizzasWithMeat);
 			pizzasWithMozzarelaAndMushrooms = PizzasRepacker.getPizzasWithMozzarelaAndMushrooms(pizzas);
+
+			CalculatePercentages();
+			FindCheapestPizzas();
+		}
+
+		public void CalculatePercentages()
+		{
+			PizzasWithMeatPercentage = (pizzasWithMeat.Count / (double)pizzas.Count) * 100;
+			PizzasWithMultipleCheasePercentage = (pizzasWithMultipleChease.Count / (double)pizzas.Count) * 100;
+			PizzasWithMeatAndOlivesPercentage = (pizzasWithMeatAndOlives.Count / (double)pizzas.Count) * 100;
+			PizzasWithMozzarelaAndMushroomsPercentage = (pizzasWithMozzarelaAndMushrooms.Count / (double)pizzas.Count) * 100;
+		}
+
+		public void FindCheapestPizzas()
+		{
+			
 		}
 	}
 }
